19fbc96872713534333d4cae9a52783aef577e6d
Pablo Perez De Angelis <pablo@pablopda.com.ar>
2010-03-29 17:55:39 -0300

    Agregado control sobre la existencia del directorio donde se hace pull de todos los proyectos

--- repos_pull_them_all.sh
+++ repos_pull_them_all.sh
@@ -3,6 +3,11 @@ GIT_DOMAIN=lvkgit@git.lavandaink.com.ar
 GIT_DIR=git.lavandaink.com.ar
 GIT_LOCAL_DIR=/home/arkat/lvk/reposborrar
 
+if [ ! -d $GIT_LOCAL_DIR ]
+  then
+      echo "the folder '$GIT_LOCAL_DIR' doesnt exist"
+      exit
+fi
 cd $GIT_LOCAL_DIR
 for repo in `ls`
 do
