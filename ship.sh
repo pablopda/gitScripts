5ae12f53c2cf9fbb8517da3f0a332abf1a8c9365
Pablo Perez De Angelis <pablo@pablopda.com.ar>
2010-04-16 20:07:50 -0300

    Compila la aplicacion y ship aborta si hay error

--- ship.sh
+++ ship.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -x
 # git name-rev is fail
+xcodebuild 2> error.log > /dev/null
+if [ ! -s "error.log" ]; then
+	#echo "all ok"
+	#exit
+else
+	echo "Compilation error"
+	echo "aborting ship"
+	exit
+fi
 CURRENT=`git branch | grep '\*' | awk '{print $2}'`
 git checkout master
 git merge ${CURRENT}
