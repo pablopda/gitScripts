6326bc66eb515a895378098eb5d6e022fe540149
Pablo Perez De Angelis <pablo@pablopda.com.ar>
2010-04-16 18:38:33 -0300

    agregado

--- repos_get.sh
+++ repos_get.sh
@@ -2,7 +2,7 @@
 #GIT_DOMAIN=arkat@localhost
 GIT_DOMAIN=lvkgit@git.lavandaink.com.ar
 GIT_DIR=git.lavandaink.com.ar
-GIT_LOCAL_DIR=/home/arkat/lvk/repos
+GIT_LOCAL_DIR=$HOME/lvk/repos
 # REPOS=: `ssh $GIT_DOMAIN 'ls '$GIT_DIR' | grep .git' ` 
 
 #funcionts
