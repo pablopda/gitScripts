d2dc56e0593e7674bf2c8eccf8cad506835920b9
Pablo Perez De Angelis <pablo@pablopda.com.ar>
2010-04-16 23:20:02 -0300

    ShipXcode antes de hacer ship compila y solo sigue si todo ok

--- shipXcode.sh
+++ shipXcode.sh
@@ -1,15 +1,33 @@
 #!/bin/sh -x
 # git name-rev is fail
-xcodebuild 2> error.log > /dev/null
-if [ ! -s "error.log" ]; then
-	#echo "all ok"
-	#exit
-else
-	echo "Compilation error"
-	echo "aborting ship"
-	exit
-fi
 CURRENT=`git branch | grep '\*' | awk '{print $2}'`
+# '/Users/arkatbrown/lvk/proyects/gitScripts'
+#GITPATH=`pwd|rev|awk -F \/ '{print $1}'|rev`
+GITNAME=`basename $PWD`
+#check if projet compila
+
+echo $GITNAME
+mkdir -p tmp/
+cd tmp
+
+#git clone -b $CURRENT /Users/arkatbrown/lvk/proyects/gitScripts
+git clone -b $CURRENT ../../$GITNAME
+
+cd $GITNAME
+
+xcodebuild 2> error.log > /dev/null
+
+if [ ! -s "error.log" ]; then
+        echo "all ok"
+        exit
+else
+        echo "Compilation error"
+        echo "aborting ship"
+        exit
+fi
+
+cd ..
+
 git checkout master
 git merge ${CURRENT}
 git push origin master
